7a03b2ffb2b7b6806d8592ae2915510241485b15
mikagitti <mikagitti@gmail.com>
2024-03-20 17:24:20 +0200

    Showing and manage shopping list departed - Added page for managing shopping list - Added page for shoving shopping list

---
+++ src/app/shoppinglist_v2/[id]/manageshoppinglist/page.tsx
@@ -0,0 +1,144 @@
+'use client'
+import React, { useEffect, useState } from "react";
+
+import { Box, Button, Typography } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+import { 
+    GetShoppingListProductsByShoppingListId, 
+    ShoppingListProductsType, 
+    RemoveProductFromShoppingList, 
+    GetAllProducts, 
+    ProductType, 
+    AddNewProductToShoppingList, 
+} from "@/Database/dbConnectionV2";
+
+import { BoxStyle, ButtonStyle } from "../../components/Styles";
+import ProductListItem, { iconType } from "../../components/productListItem";
+
+export default function Page({ params }: { params: { id: number } }) {
+
+    const IconPropsForAddingProduct : iconType = {
+        icon: AddShoppingCartIcon,
+        color: 'green',
+    };
+
+    const IconPropsForDeletingProduct : iconType = {
+        icon: DeleteIcon,
+        color: 'red',    
+    };
+      
+    const shoppingListId : number = params.id;
+
+    const [shoppingListProducts, setShoppingListProducts] = useState<ShoppingListProductsType[]>([]);        
+    const [allProducts, setAllProducts] = useState<ProductType[]>([]);
+    const [addingProduct, setAddingProduct] = useState<boolean>(false);    
+
+    useEffect( () => {        
+        fetchShoppingListProductsToMemory();                
+    }, []);
+
+    useEffect( () => {
+        fetchAwailableProductsToMemory();        
+    }, [shoppingListProducts]);
+
+    
+    //Open/Close adding more products to shopping list
+    const openAddProduct = async() => {
+        if(!addingProduct) {            
+            const productList : ProductType[] = await GetAllProducts();
+            const filteredProducts: ProductType[] = productList.filter(product => !shoppingListProducts.some(b => b.product_id === product.id));            
+            setAllProducts(filteredProducts);
+        }
+        setAddingProduct(!addingProduct);
+    }
+
+
+    const fetchShoppingListProductsToMemory = async() => {        
+        const shoppingListProducts : ShoppingListProductsType[] = await GetShoppingListProductsByShoppingListId(params.id);
+        setShoppingListProducts(shoppingListProducts);        
+    }
+
+    const fetchAwailableProductsToMemory = () => {        
+        const filteredProducts: ProductType[] = allProducts.filter(product => !shoppingListProducts.some(b => b.product_id === product.id));            
+        setAllProducts(filteredProducts);        
+    }
+
+    //REMOVE
+    const removeProductFromList = async(id : number) => {
+        await RemoveProductFromShoppingList(id);
+        await fetchShoppingListProductsToMemory();
+    }
+
+    //ADD
+    const addProductToShoppingList = async(id: number) => {
+        await AddNewProductToShoppingList(shoppingListId, id);
+        await fetchShoppingListProductsToMemory();        
+    }
+
+    return (
+        <>
+        <Box sx={ {
+            display: "flex",            
+            flexDirection: 'column',
+            textAlign: "center",
+        }}
+        > 
+            <Typography variant="h3">
+                Manage shopping list.  
+            </Typography>
+            <Typography variant="body1">
+                Add and remove products in shopping list  
+            </Typography>
+        </Box>
+
+        {
+        shoppingListProducts.length <= 0 && (
+        <>
+            <Box sx={BoxStyle} m={3}> <Typography variant="h4" component="div"> Shopping list is empty. Add products</Typography></Box>
+        </>
+        )
+        }      
+        
+        <Box sx={BoxStyle} m={1}>            
+            <Button 
+                color="primary" 
+                sx={ButtonStyle}
+                onClick={openAddProduct}
+            >
+                {addingProduct ? 'Close' : 'Add products from here' }                
+            </Button>
+        </Box>
+        
+        {
+        addingProduct ? (
+            <Box sx={ {maxWidth: '500px'}}>
+                {
+                allProducts.map((product, index) => {
+                    return (
+                        <div key={index} style={ {margin: '10px'}}>
+                            <ProductListItem icon={IconPropsForAddingProduct} name={product.name} iconAction={() => addProductToShoppingList(product.id)} />
+                        </div>
+                    )
+                })
+                }
+            </Box>
+        ):(
+            
+            <Box sx={ {maxWidth: '500px'}}>
+            {            
+                shoppingListProducts.map((product, index) => {
+                    return (
+                        <div key={index} style={ {margin: '10px'}}>
+                            <ProductListItem icon={IconPropsForDeletingProduct} name={product.name} iconAction={() => removeProductFromList(product.id)} />
+                        </div>                    
+                    )
+                })
+            }
+            </Box>            
+        )}
+
+        </>
+    )
+}
--- src/app/shoppinglist_v2/components/ShoppingListCard.tsx
+++ src/app/shoppinglist_v2/components/ShoppingListCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import EditIcon from '@mui/icons-material/Edit';
+import Link from 'next/link'
 
-import { CardContent, Paper, Typography, styled, Card, CardActions, Button, Link, Icon, IconButton } from '@mui/material';
+import { CardContent, Paper, Typography, styled, Card, CardActions, Button, Icon, IconButton } from '@mui/material';
 import { ShoppingListType } from '@/Database/dbConnectionV2';
 
 
@@ -28,23 +29,34 @@ export default function ShoppingListCard({shoppingList, deleteShoppingList, edit
         <CardContent>   
         
             <Box display="flex" flexDirection={'row'}>
-              <Typography variant="h5" >
-                {shoppingList.name}
-              </Typography>
+            
+              <Link href={`/shoppinglist_v2/${shoppingList.id}`}>                
+                <Typography variant="h5">
+                  {shoppingList.name}
+                </Typography>
+              </Link>
+
               <Box sx={ {marginTop: '-10px'}}>
                 <IconButton onClick={() => edit()} color="primary" >
                   <EditIcon fontSize='small' />
                 </IconButton>
               </Box>
+
             </Box>
             
         </CardContent>
+ 
         <CardActions >        
-          <Link href={`/shoppinglist_v2/${shoppingList.id}`}>     
-            <Button>Edit</Button>
+          <Link href={`/shoppinglist_v2/${shoppingList.id}/manageshoppinglist`}>     
+            <Button>
+              Edit
+            </Button>
           </Link>
-          <Button onClick={() => deleteShoppingList()}>Delete</Button>        
+          <Button onClick={() => deleteShoppingList()}>
+            Delete
+          </Button>        
         </CardActions>        
+
       </Card>
     </Box>
   );
